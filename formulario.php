c95658c1af0b89bf563af71dec68a8e077cefbe9
CrisTafRos <cristian.tafolla.rosales@gmail.com>
2019-03-21 10:50:15 -0600

    ADD: se agrega nuevo form de sitio2

---
+++ formulario.php
@@ -0,0 +1,18 @@
+<DOCTYPE html>
+<html>
+<meta charset="utf-8">
+	<body>	
+		<p>Favor de ingresar los siguientes datos:</p>
+		<form name="alta" action="alta.php" method="post">
+			<label for="nombre">Nombre:</label>
+			<input type="text" name="nombre"></br>
+			<label for="apaterno">Apellido paterno:</label>
+			<input type="text" name="apaterno"></br>
+			<label for="amaterno">Apellido materno:</label>
+			<input type="text" name="amaterno"></br>
+			<label for="email"> Correo electronico:</label>
+			<input type="text" name="correoe"></br>
+			<input type="submit" value="Submit">
+		</form>
+	</body>
+</html>
